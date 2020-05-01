4409307146cba9698c2a574b2366c559daa9a429
hadhemi123 <50137307+hadhemi123@users.noreply.github.com>
2020-05-01 15:56:30 +0200

    Add files via upload

---
+++ src/app/components/register/login/login.component.ts
@@ -0,0 +1,40 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, Validators,FormGroup, FormBuilder } from '@angular/forms';
+
+
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+
+
+
+
+
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  registerForm : FormGroup;
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  hide = true;
+
+
+  ngOnInit() {
+
+  }
+
+}
